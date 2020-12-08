5f3afba64967be3a7a900a4bb761fe2c4f84a9b2
kevinmora47 <kevinmora47@gmail.com>
2020-12-07 21:43:13 -0600

    Events

---
+++ contollers/main/main.js
@@ -0,0 +1,9 @@
+
+    $(document).ready(function () {
+        $('#header-form').css('display', 'none');
+        $('#chart').css('width', '100vw;');
+        $('#headerButton').click(function () {
+            $('#chart').css('display', 'none');
+        });
+    });
+
