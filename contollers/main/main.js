20ad3ac5b7e73c054daf33b22db80e495eb4c7b0
kevinmora47 <kevinmora47@gmail.com>
2020-12-08 20:28:48 -0600

    added of panel actions

--- contollers/main/main.js
+++ contollers/main/main.js
@@ -1,9 +1,23 @@
+$(document).ready(function () {
+  $("#headerButton").click(function () {
+    showHeaderButton();
 
-    $(document).ready(function () {
-        $('#header-form').css('display', 'none');
-        $('#chart').css('width', '100vw;');
-        $('#headerButton').click(function () {
-            $('#chart').css('display', 'none');
-        });
-    });
+  });
+});
 
+function showHeaderButton() {
+  $("#chart").addClass("hide");
+  $("#header-form").addClass("activate-header-form");
+  $("#header-form-title").addClass("card-title xl");
+  $("#header-form").addClass("activate-header-form");
+  $("#lbl-name-header").addClass("profile-title");
+  $("#header-form").addClass("header-form");
+  $("#header-form-container").addClass("header-form-container");
+  $("#input").addClass("input");
+  $("#header-title").text("Editing Header");
+  $("#btn-header").addClass("button");
+}
+
+function chargeHeader(){
+  
+}
