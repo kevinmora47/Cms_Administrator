03c4a18789d584ef9b752209f68c77a0c885d1c9
kevinmora47 <kevinmora47@gmail.com>
2020-12-09 19:20:13 -0600

    part 1

--- contollers/main/main.js
+++ contollers/main/main.js
@@ -1,11 +1,7 @@
 // Start of the clicks events.
 document.getElementById("headerButton").onclick = function (e) {
   showHeaderButton();
-  chargeHeader();
-}
-
-document.getElementById("btn-header").onclick = function (e) {
-  editHeader();
+  // chargeHeader();
 }
 
 
@@ -28,33 +24,33 @@ function showHeaderButton() {
   $("#btn-header").addClass("button");
 }
 
-async function chargeHeader() {
-  let response = await fetch("https://localhost:5001/api/Banner");
-  let data = await response.json();
-  document.getElementById("input").value = data[0].header;
-  document.getElementById("id-banner").value = data[0].idBanner;
-}
+// async function chargeHeader() {
+//   let response = await fetch("https://localhost:5001/api/Banner");
+//   let data = await response.json();
+//   document.getElementById("input").value = data[0].header;
+//   document.getElementById("id-banner").value = data[0].idBanner;
+// }
 
-async function editHeader() {
+// async function editHeader() {
 
-  fetch("https://localhost:5001/api/Banner", {
-    method: "PUT",
-    body: JSON.stringify({
-      idBanner: document.getElementById("id-banner").value,
-      header: document.getElementById("input").value
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then(
-    Swal.fire({
-      width: '30%',
-      height: '50%',
-      icon: 'success',
-      title: 'Your work has been saved',
-      showConfirmButton: false,
-      timer: 2000
-    }))
-    .then(json => console.log(json));
+//   fetch("https://localhost:5001/api/Banner", {
+//     method: "PUT",
+//     body: JSON.stringify({
+//       idBanner: document.getElementById("id-banner").value,
+//       header: document.getElementById("input").value
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8"
+//     }
+//   }).then(
+//     Swal.fire({
+//       width: '30%',
+//       height: '50%',
+//       icon: 'success',
+//       title: 'Your work has been saved',
+//       showConfirmButton: false,
+//       timer: 2000
+//     }))
+//     .then(json => console.log(json));
 
-}
+// }
