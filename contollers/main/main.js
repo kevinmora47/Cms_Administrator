fe1126bd72d156484d3b76f4c06eca38b3a64b43
kevinmora47 <kevinmora47@gmail.com>
2020-12-10 20:00:18 -0600

    added of controls

--- contollers/main/main.js
+++ contollers/main/main.js
@@ -1,14 +1,16 @@
 // Start of the clicks events.
 document.getElementById("headerButton").onclick = function (e) {
-  showHeaderButton();
-  // chargeHeader();
+  showHeaderPanel();
 }
-
+document.getElementById("exchangeButton").onclick = function (e) {
+  showExchagePanel();
+}
 
 // End of the clicks events.
 
-function showHeaderButton() {
+function showHeaderPanel() {
   $("#chart").addClass("hide");
+  $("#header-form").addClass("hide");
   $("#header-form").addClass("activate-header-form");
   $("#header-form-title").addClass("card-title xl");
   $("#header-form").addClass("activate-header-form");
@@ -24,33 +26,11 @@ function showHeaderButton() {
   $("#btn-header").addClass("button");
 }
 
-// async function chargeHeader() {
-//   let response = await fetch("https://localhost:5001/api/Banner");
-//   let data = await response.json();
-//   document.getElementById("input").value = data[0].header;
-//   document.getElementById("id-banner").value = data[0].idBanner;
-// }
+function showExchagePanel() {
+  $("#header-title").text("Editing Exchange");
+  $("#chart").addClass("hide");
+  $("#header-form").addClass("hide");
+  $("#exchange-form").addClass("header-form-container");
+  $("#exchange-form").addClass("activatee-header-form");
 
-// async function editHeader() {
-
-//   fetch("https://localhost:5001/api/Banner", {
-//     method: "PUT",
-//     body: JSON.stringify({
-//       idBanner: document.getElementById("id-banner").value,
-//       header: document.getElementById("input").value
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8"
-//     }
-//   }).then(
-//     Swal.fire({
-//       width: '30%',
-//       height: '50%',
-//       icon: 'success',
-//       title: 'Your work has been saved',
-//       showConfirmButton: false,
-//       timer: 2000
-//     }))
-//     .then(json => console.log(json));
-
-// }
+}
